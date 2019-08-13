f4aadbc0ac9b35747adc3e42c5ce6063a5690c9b
atdaunais <atdaunais@gmail.com>
2019-08-13 12:41:32 -0500

    Adjusted the second problem so it could be solved with a for loop

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ let studentNames = ["Andrew", "Demetria", "Hamida", "Bruno"];
 console.log(studentNames);
 
 let sevenMultiples = [7, 14, 21, 28, 35];
-console.log(sevenMultiples[0]);
-console.log(sevenMultiples[1]);
-console.log(sevenMultiples[2]);
-console.log(sevenMultiples[3]);
-console.log(sevenMultiples[4]);
+for(let eachElement in sevenMultiples)
+{
+    console.log(sevenMultiples[eachElement]);
+}
 
 let familyNames = ["Andrew", "Michael", "Bridget", "John"];
 console.log(familyNames[2]);
