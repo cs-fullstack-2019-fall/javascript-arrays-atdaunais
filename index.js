d8174c4b080dc42946fdfb555499299bba3caec8
atdaunais <atdaunais@gmail.com>
2019-08-13 11:42:43 -0500

    Completed and tested the graded assignment.

---
+++ index.js
@@ -0,0 +1,15 @@
+let studentNames = ["Andrew", "Demetria", "Hamida", "Bruno"];
+console.log(studentNames);
+
+let sevenMultiples = [7, 14, 21, 28, 35];
+console.log(sevenMultiples[0]);
+console.log(sevenMultiples[1]);
+console.log(sevenMultiples[2]);
+console.log(sevenMultiples[3]);
+console.log(sevenMultiples[4]);
+
+let familyNames = ["Andrew", "Michael", "Bridget", "John"];
+console.log(familyNames[2]);
+familyNames.splice(1, 1);
+console.log(familyNames); //check to make sure second item was removed
+console.log(familyNames[2]);
